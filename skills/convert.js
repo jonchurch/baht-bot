2c9aa66b133ad6d36e4a7a2cb12848714609795b
jonchurch <me@jonchurch.com>
2017-09-26 23:32:21 +0700

    add currency formatter

--- skills/convert.js
+++ skills/convert.js
@@ -7,6 +7,31 @@ module.exports = (controller) => {
 		const latest = await rp.get('https://api.fixer.io/latest?base=THB')
 		const usdR = latest.rates.USD
 		
-		bot.reply(message, `USD: ${message.value * usdR }`)
+		bot.reply(message, `${formatCurrency(message.value * usdR)}`)
 	})
+}
+
+function formatCurrency(num)
+{
+    num = num.toString().replace(/\$|\,/g, '');
+    if (isNaN(num))
+    {
+        num = "0";
+    }
+
+    sign = (num == (num = Math.abs(num)));
+    num = Math.floor(num * 100 + 0.50000000001);
+    cents = num % 100;
+    num = Math.floor(num / 100).toString();
+
+    if (cents < 10)
+    {
+        cents = "0" + cents;
+    }
+    for (var i = 0; i < Math.floor((num.length - (1 + i)) / 3); i++)
+    {
+        num = num.substring(0, num.length - (4 * i + 3)) + ',' + num.substring(num.length - (4 * i + 3));
+    }
+
+    return (((sign) ? '' : '-') + '$' + num + '.' + cents);
 }
